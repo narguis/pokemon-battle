163d64278428a296162ac2acf023c7008d583174
narguis <narguis.guilherme@gmail.com>
2023-08-23 00:23:05 -0300

    Pokémon added

---
+++ src/components/PokemonSelector.js
@@ -0,0 +1,12 @@
+
+function PokemonSelector(props){
+    return(
+        <div>
+            <button onclick={alert('oi')}>Charmander</button>
+            <button>Squirtle</button>
+            <button>Bulbasaur</button>
+        </div>
+    )
+}
+
+export default PokemonSelector
