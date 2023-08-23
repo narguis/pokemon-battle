4082b4e6bae64173921875538ab054e7cde841b5
narguis <narguis.guilherme@gmail.com>
2023-08-23 17:34:01 -0300

    Fighting screen and move objects added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import PokemonSelector from './components/PokemonSelector';
 import Pokemon from './components/Pokemon';
+import FightScreen from './components/FIghtScreen';
 
 function App() {
   return (
     <div className="App">
-      <Pokemon pokemonname='charmander'/>
+      <FightScreen/>
     </div>
   );
 }
--- src/components/FIghtScreen.js
+++ src/components/FIghtScreen.js
@@ -1,7 +1,18 @@
+import styles from './FightScreen.module.css'
+import Pokemon from './Pokemon'
+
 function FightScreen(){
     return(
-        <div>
-
+        <div id={styles.FightScreen}>
+            {/* <h1>Round 1</h1> */}
+            <div id={styles.Fight}>
+                <div id={styles.YourPokemon}>
+                    <Pokemon pokemonname='charmander'/>
+                </div>
+                <div id={styles.EnemyPokemon}>
+                    <Pokemon pokemonname='squirtle'/>
+                </div>
+            </div>
         </div>
     )
 }
