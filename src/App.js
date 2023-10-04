c009b7fd6bc63c7a024f3e4c5e82d3b56169fea9
narguis <narguis.guilherme@gmail.com>
2023-10-04 13:58:16 -0300

    React-router-dom applie

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import './App.css';
 import PokemonSelector from './components/PokemonSelector';
 import Pokemon from './components/Pokemon';
 import FightScreen from './components/FIghtScreen';
 import MoveButton from './components/MoveButton';
+import Charmander from './components/pokemon/Charmander';
 
 function App() {
   return (
-    <div className="App">
-      Placeholder
-    </div>
+    <Router>
+        <Routes>
+          <Route path='/FightScreen' element={<Charmander/>}/>
+        </Routes>
+        <PokemonSelector/>
+    </Router>
   );
 }
 
